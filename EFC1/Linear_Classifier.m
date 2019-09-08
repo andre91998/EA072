af320c1eb9801c000f15975a88118f3b5e5de055
André Barros de Medeiros <30624044+andre91998@users.noreply.github.com>
2019-09-08 17:14:09 -0300

    first version of MNIST linear regression classifier

---
+++ EFC1/Linear_Classifier.m
@@ -0,0 +1,49 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Author: André Barros de Medeiros
+%Date:09/05/2019
+%Description:
+%Copyright: free to use, copy, and modify
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clear all;
+%Data Matrix
+load('data.mat');
+%Parameter Matrix
+W=(-1 + 2*rand(785,10))/10;
+W(:,1)=1;
+%Mean quadratic error
+EQM=zeros(1,23);
+%counting correct classifications variable
+%certo=0;
+%percentage of correct classifications for each regfactor
+TCC=zeros(1,23);
+
+regfactorV=zeros(1,23);
+
+for i = 1:1:23
+    certo=0;
+    regfactor = 2^(i-11);
+    regfactorV(i)=regfactor;
+    Xtrain=X(1:40000,:);
+    Xval=X(40001:60000,:);
+    Strain=S(1:40000,:);
+    Sval=S(40001:60000,:);
+    %calculate parameter matrix
+    W=(inv((Xtrain.')*Xtrain+(regfactor)*(eye(784,784)))*(Xtrain.')*Strain);
+    Sc=Xval*W;
+    %EQM(i)=(norm(Sc-Sval))^2+regfactor*(norm(W))^2;
+     
+       for j = 1:1:20000
+          Ic=zeros(1,1);
+          I=zeros(1,1);
+          [Mc,Ic]=max(Sc(j,1:10),[],'linear');
+          [M,I]=max(Sval(j,1:10),[],'linear');
+         if I(1)==Ic(1)
+              certo=certo+1;
+         end
+       end
+       TCC(i)=certo/20000;
+end
+
+semilogx(regfactorV,TCC);
+
---
+++ EFC1/heat_map.m
@@ -0,0 +1,15 @@
+for n=1:1:10
+    i=1;
+    H=zeros(10,10);
+    for j=1:1:784
+        r=rem(j,28);
+        if r==0
+            i=i+1;
+            r=1;
+        end
+        H(r,i)=W(j,n);
+
+    end
+    figure(n);
+    heatmap(H,'GridVisible','off','Colormap',pink);
+end
